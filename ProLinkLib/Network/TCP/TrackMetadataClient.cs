1a61f3edd98f5547acd29bebf02a8a0b213e303a
Davidc96 <96cuadrado@gmail.com>
2023-01-04 14:24:43 +0100

    Init Tracklist functionality

---
+++ ProLinkLib/Network/TCP/TrackMetadataClient.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProLinkLib.Network.TCP
+{
+    public class TrackMetadataClient
+    {
+        int INITIAL_PORT = 15123;
+    
+    }
+}
